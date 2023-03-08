e65018789b348c4023badbe2a9f7538a1152c6c0
afeiship <1290657123@qq.com>
2023-03-08 19:26:43 +0800

    style: fmt trim

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-
-export default ( inStr:string, inObj: Record<string,string>,inDefault?: string) => {
+export default (inStr: string, inObj: Record<string, string>, inDefault?: string) => {
   const defaultVal = inDefault || '';
   const keys = Object.keys(inObj);
   const key = keys.find((key) => inStr.includes(key));
